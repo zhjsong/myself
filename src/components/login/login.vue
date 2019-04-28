a560a8203ed3c6a1d8cd3a99ff3dda14e6c5e343
zhjsong <492992198@qq.com>
2019-04-28 18:09:59 +0800

    'login样式'

---
+++ src/components/login/login.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="login-wrap">
+        
+        <el-form label-position="top" label-width="80px" :model="formdata" class="login-form">
+            <h2>用户登录</h2>
+         <el-form-item label="用户名">
+         <el-input v-model="formdata. username"></el-input>
+         </el-form-item>
+         <el-form-item label="密码">
+         <el-input v-model="formdata.password"></el-input>
+          </el-form-item>
+          <el-button type="primary" class="login-btn">主要按钮</el-button>
+</el-form>
+    </div>
+</template>
+
+<script>
+export default {
+   data(){
+       return {
+           formdata:{
+               username:'',
+               password:''
+           }
+       }
+   }
+}
+</script>
+
+<style>
+.login-wrap {
+  height: 100%;
+  background-color: #324152;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  }
+.login-wrap .login-form {
+    width: 400px;
+    background-color: #fff;
+    border-radius: 5px;
+    padding: 30px;
+}
+.login-wrap .login-form .login-btn {
+   width: 100%;
+}
+</style>
+
+
